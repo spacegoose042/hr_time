b82d77074c2d636bfb4d9c019e476855a35bcd87
spacegoose042 <94691697+spacegoose042@users.noreply.github.com>
2024-12-14 08:41:27 -0600

    updating

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
 import { Express, Router } from 'express';
+import express from 'express';
 import authRoutes from '../auth/routes/authRoutes';
 import adminRoutes from './admin.routes';
 import timeEntryRoutes from './timeEntry.routes';
+import path from 'path';
 
 export const setupRoutes = (app: Express) => {
+  // Serve static files from a public directory
+  app.use(express.static(path.join(__dirname, '../../public')));
+
   const apiRouter = Router();
 
   // Mount routes
